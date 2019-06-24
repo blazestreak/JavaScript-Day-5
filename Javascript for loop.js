19e6de1ddc858e033bf959e53b2444bd45288bb7
Luke Johnson <blazestreak@Lukes-MacBook-Pro.local>
2019-06-24 11:57:43 -0400

    Welcome

---
+++ Javascript for loop.js
@@ -0,0 +1,8 @@
+or (var i = 99; i !== 0; i--) {
+
+      if (i===0){
+console.log("Hey, we need more root beer!");
+}
+
+    console.log( i );
+}
---
+++ javascript day 5 conditionals.js
@@ -0,0 +1,21 @@
+var temp = 80;
+if(temp === 80) {
+	console.log("The temperature is 80 degrees");
+}
+
+if(temp > 80);
+{console.log("time to swim");
+}
+
+
+var precip= false
+if(precip== false)
+{
+     console.log("snowing")
+
+ }
+
+var indoors= true
+if(indoors==true) {
+console.log("time to swim");
+}
